42487a3a8cf36fc349b0ea35345230df5bb9c79f
Pavel Rakovich <prvsega@gmail.com>
2022-11-29 16:39:30 +0300

    Initial commit

---
+++ src/main/java/by/prvsega/springmvcapp/dao/PersonDAO.java
@@ -0,0 +1,73 @@
+package by.prvsega.springmvcapp.dao;
+
+import by.prvsega.springmvcapp.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Component
+public class PersonDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public int maxId() {
+        Integer i = jdbcTemplate.queryForObject("SELECT MAX(ID) FROM Person", Integer.class);
+
+        if (i == null){
+            i = 0;
+        }
+
+            return i;
+    }
+
+    public List<Person> index() {
+        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    public Person show(int id) {
+
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny().orElse(null);
+
+    }
+
+    public void save(Person person) {
+//        i = new PersonDAO(new JdbcTemplate()).maxId();
+        jdbcTemplate.update("INSERT INTO Person VALUES (?, ?, ?, ?)", maxId() + 1, person.getName(), person.getAge(), person.getEmail());
+
+
+    }
+
+    public void update(int id, Person updatedPerson) {
+
+        jdbcTemplate.update("UPDATE Person SET name=?, age=?, email=? WHERE id=?",
+                updatedPerson.getName(), updatedPerson.getAge(), updatedPerson.getEmail(), id);
+
+
+    }
+
+    public void delete(int id) {
+//        Person personToBeDelete = show(id);
+//        people.remove(personToBeDelete.getId());
+        /*        people.removeIf(p -> p.getId() == id);*/
+        jdbcTemplate.update("DELETE FROM  Person WHERE id=?", id);
+
+    }
+
+    public List<Person> search(String string){
+
+        return jdbcTemplate.query("SELECT * FROM Person WHERE name=?", new Object[]{string} , new BeanPropertyRowMapper<>(Person.class));
+
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<p>There are many books in website, your books.</p>
+<p>This is list with your liked books.</p>
+<p>You can to creat new books, to read information, to update information about books (Maybe you read something book) and to delete books ( If book is damn bad or you watched film).</p>
+<p>There is <a href="/people">list by link</a></p>
+</body>
+</html>
