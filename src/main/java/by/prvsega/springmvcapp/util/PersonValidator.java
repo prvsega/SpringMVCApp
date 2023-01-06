3b0a7e67ed684ffd95755c8e7a1b6392a763d8cb
Pavel Rakovich <prvsega@gmail.com>
2023-01-06 11:40:58 +0300

    Lesson 30-31. I studied SpringValidator ( @Pattern) and select-options ( html + thymeleaf)

---
+++ src/main/java/by/prvsega/springmvcapp/util/PersonValidator.java
@@ -0,0 +1,35 @@
+package by.prvsega.springmvcapp.util;
+
+import by.prvsega.springmvcapp.dao.PersonDAO;
+import by.prvsega.springmvcapp.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class PersonValidator implements Validator {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PersonValidator(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return Person.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Person person = (Person) o;
+
+        if (personDAO.show(person.getEmail()) != null){
+            errors.rejectValue("email", "", "This email isn't unique");
+        }
+
+
+    }
+}
